ed8bed401c3652882a1055492098a3c5ed587050
HaiPTIT <haivtndc@gmail.com>
2020-12-11 10:24:01 +0700

    add swagger ui

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,38 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Prospect Api Demo Documentation",
+     *      description="Prospect API demo description",
+     *      @OA\Contact(
+     *          email="hai.vu@twentyci.asia"
+     *      ),
+     *      @OA\License(
+     *          name="TwentyTech",
+     *          url="https://www.twentyci.co.uk/"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Demo API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Prospect",
+     *     description="API Endpoints of Projects"
+     * )
+     *
+     * /**
+     * @OA\Get(
+     *     path="/",
+     *     description="Home page",
+     *     @OA\Response(response="default", description="Welcome page")
+     * )
+     */
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
